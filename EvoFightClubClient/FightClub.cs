d37c6ef6fde10ac6735401acf0062eb957ec369c
CrookedBadge <rrromanhouse@gmail.com>
2015-12-18 23:50:55 +0100

    First

---
+++ EvoFightClubClient/FightClub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace EvoFightClubClient
+{
+    class FightClub
+    {
+        public List<Fighter> fighters { get; }
+        private string log;
+
+        public FightClub(ref string log)
+        {
+            this.log = log;
+        }
+        
+    }
+}
